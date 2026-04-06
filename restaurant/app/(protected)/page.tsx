198e9c83ccb3a83fbc786f6b6fe993f342d54e38
LEONARDO LUCAS DE BRITO SILVA <163042516+leonardolucasbs@users.noreply.github.com>
2026-04-06 10:41:15 -0300

    init :tada: - initial commit

---
+++ restaurant/app/(protected)/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main className="min-h-screen flex justify-center items-start pt-4">
+        <h1 className="px-4 py-2 font-bold">Menu</h1>
+        
+      </main>
+    </div>
+  );
+}
---
+++ restaurant/app/components/Header.tsx
@@ -0,0 +1,14 @@
+"use client";
+export default function Header() {
+  return (
+    <header className="bg-gray-500 text-white   p-4">
+      <header>
+        <ul className="font-bold align-middle flex gap-4">
+          <li>
+            <a href="/page">Restaurant</a>
+          </li>
+        </ul>
+      </header>
+    </header>
+  );
+}

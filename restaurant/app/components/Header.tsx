963cf6546a816851af9b1d602bd8a68abb044ab5
LEONARDO LUCAS DE BRITO SILVA <163042516+leonardolucasbs@users.noreply.github.com>
2026-04-09 17:39:23 -0300

    remove :wastebasket: - deleting unnecessary folders

--- restaurant/app/components/Header.tsx
+++ restaurant/app/components/Header.tsx
@@ -2,13 +2,11 @@
 export default function Header() {
   return (
     <header className="bg-gray-500 text-white   p-4">
-      <header>
-        <ul className="font-bold align-middle flex gap-4">
-          <li>
-            <a href="/page">Restaurant</a>
-          </li>
-        </ul>
-      </header>
+      <ul className="font-bold align-middle flex gap-4">
+        <li>
+          <a href="/page">Restaurant</a>
+        </li>
+      </ul>
     </header>
   );
 }
--- restaurant/app/page.tsx
+++ restaurant/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "../components/Header";
-import Menu from "../components/Menu";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
 
 export default function Home() {
   return (
